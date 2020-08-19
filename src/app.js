a9d313ddcbd0cd218c29068d643fe4bb9b09477e
johannamag <69107842+johannamag@users.noreply.github.com>
2020-08-19 19:47:13 +0200

    Added Forecast

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 function formateDate(timestamp) {
   let date = new Date(timestamp);
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return `${day}, ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -8,9 +16,8 @@ function formateDate(timestamp) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  let day = days[date.getDay()];
-  return `${day}, ${hours}:${minutes}`;
+
+  return `${hours}:${minutes}`;
 }
 
 function showTemperature(response) {
@@ -36,10 +43,32 @@ function showTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function showForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = 0;
+
+  for (let index = 0; index < 6; index++) {
+    let forecast = response.data.list[index];
+    forecastElement.innerHTML += `<div class="col-2">
+              <h3>12:00</h3>
+              <img src="http://openweathermap.org/img/wn/${
+                forecast.weather[0].icon
+              }@2x.png" alt="" />
+              <div class="forecast-temp"><strong>${Math.round(
+                forecast.main.temp_max
+              )}°</strong>${Math.round(forecast.main.temp_min)}°</div>
+            </div>`;
+  }
+}
+
 function search(city) {
   let apiKey = "ab96d1f59a7ee590668ddadbffe2617d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function handleSubmit(event) {
